36e895c71702627f27b982d29573fddbb0f2882b
valdo <osvaldo@lvvdigital.com>
2024-06-06 12:42:06 -0600

    feat: inster to mongo

--- pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePokemonDto {}
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @Min(1)
+  no: number;
+}
--- pokedex/src/pokemon/pokemon.service.ts
+++ pokedex/src/pokemon/pokemon.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(@Body() createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+    const pokemon = await this.pokemonModel.create(createPokemonDto);
+    return pokemon;
   }
 
   findAll() {
