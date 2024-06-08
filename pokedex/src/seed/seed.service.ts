21526feb259c24f9be11ab0e3cc46cb7bd21c235
osvaldo hinojosa <osvaldo.javier14@gmail.com>
2024-06-08 11:31:27 -0600

    feat: antes de insertar por lote

--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  execute() {
-    return 'Seed executed!';
+  private readonly axios: AxiosInstance = axios;
+
+  async execute() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=3',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+    });
+
+    return data.results;
   }
 }
